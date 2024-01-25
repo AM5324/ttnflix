01278ec8495142a11d513c1cfeb76f081a8eab9d
Atul Mishra <atul.mishra@tothenew.com>
2024-01-26 00:02:13 +0530

    commit with Account setup.

---
+++ lib/data/models/user.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      password: json['password'] as String?,
+      dob: json['dob'] as String?,
+      gender: json['gender'] as String?,
+      username: json['username'] as String?,
+    );
