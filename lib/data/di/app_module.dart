d7043eb0e4745582d28dae9046f0b0fea35feb26
Atul Mishra <atul.mishra@tothenew.com>
2024-01-25 23:03:54 +0530

    commit with shared_prefernce setup.

--- lib/data/di/app_module.dart
+++ lib/data/di/app_module.dart
@@ -1,9 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ttn_flix/data/di/ttn_flix_service_locator.dart';
 import 'package:ttn_flix/data/di/ttn_flix_service_locator_imple.dart';
 
 import '../network/dio_api_client.dart';
 import '../network/dio_service.dart';
+import '../shared_prefernce/shared_prefernce_manager.dart';
 
 class AppModule {
   Future<void> initialise() async {
@@ -25,6 +27,12 @@ class AppModule {
     // Dio Http Client
     if (!serviceLocator.isRegistered<DioApiClient>()) {
       serviceLocator.registerSingleton(DioApiClient());
+    }
+
+    // Register SharedPreferencesManager as a singleton
+    if (!serviceLocator.isRegistered<SharedPreferencesManager>()) {
+      await SharedPreferencesManager.getInstance(); // Ensure it's initialized
+      serviceLocator.registerSingletonAsync(() => SharedPreferencesManager.getInstance());
     }
   }
 }
---
+++ lib/data/shared_prefernce/shared_prefernce_manager.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_it/get_it.dart';
+
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_it/get_it.dart';
+
+import '../di/ttn_flix_service_locator.dart';
+
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesManager {
+  static SharedPreferencesManager? _instance;
+  static SharedPreferences? _preferences;
+
+  SharedPreferencesManager._();
+
+  static Future<void> _init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  static Future<SharedPreferencesManager> getInstance() async {
+    _instance ??= SharedPreferencesManager._(); // Change here: Initialize if null
+    await _init(); // Move the initialization inside getInstance method
+    return _instance!; // Change here: Add a non-null assertion
+  }
+
+  // Save and retrieve String
+  String getString(String key, {required String defaultValue}) {
+    return _preferences?.getString(key) ?? defaultValue;
+  }
+
+  Future<void> saveString(String key, String value) async {
+    await _preferences?.setString(key, value);
+  }
+
+  // Save and retrieve JSON using a model
+  T? getObject<T>(String key, T Function(Map<String, dynamic>) fromJson) {
+    final jsonString = _preferences?.getString(key);
+    if (jsonString != null) {
+      final Map<String, dynamic> jsonMap = json.decode(jsonString);
+      return fromJson(jsonMap);
+    }
+    return null;
+  }
+
+  Future<void> saveObject(String key, dynamic object) async {
+    final jsonString = json.encode(object.toJson());
+    await _preferences?.setString(key, jsonString);
+  }
+
+  // Save and retrieve a list of JSON using a model
+  List<T> getList<T>(String key, T Function(Map<String, dynamic>) fromJson) {
+    final jsonStringList = _preferences?.getStringList(key);
+    if (jsonStringList != null) {
+      return jsonStringList.map((jsonString) {
+        final Map<String, dynamic> jsonMap = json.decode(jsonString);
+        return fromJson(jsonMap);
+      }).toList();
+    }
+    return [];
+  }
+
+  Future<void> saveList(String key, List<dynamic> list) async {
+    final jsonStringList = list.map((item) => json.encode(item)).toList();
+    await _preferences?.setStringList(key, jsonStringList);
+  }
+}
+
